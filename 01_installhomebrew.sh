ca90ede029a12fd05bdb80ed669d00aabd754332
Matthew Baker <matthewbaker@hestia.attlocal.net>
2019-10-20 10:35:33 -0500

    changes for zsh

--- 01_installhomebrew.sh
+++ 01_installhomebrew.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/local/bin/zsh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
--- 02_installbash4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-brew install bash
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
