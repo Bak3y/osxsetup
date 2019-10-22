04fbd92550b947badb3c39fc08e5ae002018cf30
Matt Baker <mbaker@shipt.com>
2019-10-22 09:20:01 -0500

    add vscode extensions

---
+++ 05_installvscodexts.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/zsh
+
+#only run this after your dotfiles are configured, especially to use the "code" alias
+code --install-extension eamodio.gitlens
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-python.python
+code --install-extension ms-vscode.Go
+code --install-extension redhat.vscode-yaml
+code --install-extension ronnidc.nunjucks
+code --install-extension timonwong.shellcheck
+code --install-extension zxh404.vscode-proto3
